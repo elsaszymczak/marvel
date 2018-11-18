0b2e69b2261c2a1b997482b4f366f4c3806c40a0
Elsa Szymczak <elsaszymczak@gmail.com>
2018-11-18 11:35:48 +0800

    routes for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :events, only: [:index, :show] do
+    resources :characters, only: [:index, :show]
+    resources :comics, only: [:index, :show]
+  end
 end
